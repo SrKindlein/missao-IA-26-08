32bfd24f9833c93eab2c5969e450ede6a1d1baa4
SrKindlein <adrian.kindlein@escola.pr.gov.br>
2025-08-26 11:48:59 -0300

    Add files via upload

---
+++ miss-o-IA-19-08-main/miss-o-IA-12-08-main/js/perguntas.js
@@ -0,0 +1,96 @@
+export const perguntas = [
+    {
+        enunciado: "como economizar mais energia?",
+        alternativas: [
+            {
+            
+            texto: "deixando os aparelhos em stand-by ",
+            afirmacao: [
+            "voce errou",
+            
+         ]
+            },
+            {
+            texto: " deixando os aparelhos desligados",
+            afirmacao: [
+                "você acertou em cheio parabéns",
+                
+            ]
+            },
+            
+        ]
+    },
+    {
+        enunciado: "Quantos continentes existem no mundo?",
+        alternativas: [
+            {
+                texto: "6",
+                afirmacao: "voce errou",
+            },
+            {
+                texto: "7",
+                afirmacao: [
+                    " voce acertou ",
+                    
+                ]
+            },
+        ]
+    },
+    {
+        enunciado: "Qual é a bebida mais consumida no mundo?",
+        alternativas: [
+            {
+                texto: "cha",
+                afirmacao: [
+                    " voce acertou",
+                    
+                ]
+            },
+            {
+                texto: "cerveja",
+                afirmacao: [
+                    "voce errou",
+                    
+                ]
+            },
+        ]
+    },
+    {
+        enunciado: "Em que ano o Brasil se tornou independente de Portugal?",
+        alternativas: [
+            {
+                texto: "1500",
+                afirmacao: [
+                    "voce errou",
+                    
+                ]
+            },
+            {
+                texto: "1822",
+                afirmacao: [
+                    "voce acertou",
+                    
+                ]
+            }
+        ]
+    },
+    {
+        enunciado: "Qual é a cor do cavalo branco de Napoleão?",
+        alternativas: [
+            {
+                texto:"branco",
+                afirmacao: [
+                    "voce acertou",
+                    
+                ]
+            },
+            {
+                texto: "preto ",
+                afirmacao: [
+                    "voce errou",
+                    
+                ] 
+            }
+        ]
+    },
+]
